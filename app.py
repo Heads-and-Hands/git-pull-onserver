d413cc2a4430aab2aa7f0d35901cb3b5bc0b6113
root <root@handh.ru>
2016-02-05 12:12:34 +0300

    Add config file

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import subprocess
+from config import repos
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello():
-    remote_name = 'origin'
-    remote_branch = 'develop'
-    cmd = ["git reset --hard && git pull %s %s" % (remote_name, remote_branch),""]
+    current_repo = repos.get('key')
+    remote_name = current_repo('remote_name')
+    remote_branch = current_repo('remote_branch')
+    local_dir = current_repo('local_dir')
+    cmd = ["cd %s && git reset --hard && git pull %s %s" % (local_dir, remote_name, remote_branch),""]
     p = subprocess.Popen(cmd, shell=True, close_fds=True, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     out,err = p.communicate()
---
+++ config.py
@@ -0,0 +1,8 @@
+#
+repos = {
+  'key': {
+    'remote_name': 	'origin',
+    'remote_branch': 	'develop',
+    'local_dir': 	'/var/www/html/flask-test'
+  } 
+} 
